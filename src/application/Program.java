95ba8ec99e46876c04e85216458d66e9770b46dd
Adroaldo <hygino@msn.com>
2020-10-13 20:22:58 -0300

    segundo teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,56 +1,73 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
 
 import entities.Product;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 		List<Product> lista = new ArrayList<>();
 		
-		String arqOrigem ="C:\\temp\\lista.txt";
-		File origem = new File(arqOrigem);
-		origem.getPath();
+		String arqOrigem ="/home/hygino/Documentos/lista.txt";
 		
+		File origem = new File(arqOrigem);
+		
+		System.out.println(origem.getPath());
+		//mostra caminho completo até o arquivo
 		System.out.print("Diretório para gravar: ");
-		String diretorio = "c:\\temp";
+		String diretorio = origem.getParent();
+		//mostra caminho completo até a pasta em que está o arquivo
 		System.out.println(diretorio);
-		//File path = new File(diretorio);
-
-		System.out.println("Source file:");
-		int n = 4;// número de produtos
-
-		for (int i = 0; i < n; i++) {
-			String produto = sc.nextLine();
-			String p[] = produto.split(",");
-			// separa a string pela vírgula
-			String name = p[0];
-			double price = Double.parseDouble(p[1]);
-			int quantity = Integer.parseInt(p[2]);
-			lista.add(new Product(name, price, quantity));
+		
+		File caminhoArquivo = new File(diretorio + "/out/summary.csv");
+		
+		//gravação do arquivo de saída
+		boolean success = new File(diretorio + "/out").mkdir();
+		// cria a pasta out
+		System.out.println("Directory created successfully: " + success);
+		
+		
+		//arquivo de saída
+		try (BufferedReader bf = new BufferedReader(new FileReader(origem))){
+			String linha = bf.readLine();
+		
+			//lê a linha
+			while (linha!= null) {
+				String p[] = linha.split(",");
+				String name = p[0];
+				double price = Double.parseDouble(p[1]);
+				int quantity = Integer.parseInt(p[2]);
+				lista.add(new Product(name, price, quantity));
+				linha = bf.readLine();
+			}
+			
 		}
+		catch (IOException e) {
+			System.out.println("Ocorreu algum erro");
+		}
+		
+		
+		
 		System.out.println("Fim da entrada de dados\n");
-		System.out.println("Output file (out/summary.csv):");
+		//System.out.println("Output file (out/summary.csv):");
 		for (Product p : lista) {
 			System.out.println(p);
 		}
-		//gravação do arquivo de saída
-		boolean success = new File(diretorio + "\\out").mkdir();
-		// cria a pasta out
-		System.out.println("Directory created successfully: " + success);
-
-		// gerar arquivo .csv
-		File caminhoArquivo = new File(diretorio + "\\out\\summary.csv");
+		
+		
+		
+		
 
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoArquivo))) {
 			for (Product line : lista) {
@@ -61,8 +78,11 @@ public class Program {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 
-		sc.close();
+		
+		System.out.print("\nArquivo gravado em: ");
+		System.out.println(caminhoArquivo.getPath());
 	}
 
 }
